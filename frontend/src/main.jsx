e1f539ddd0c088014205517e8715cc8eabd29878
Hardik Garg <hardikg2907@gmail.com>
2023-10-25 11:09:52 +0530

    basic sidebar added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import axios from "axios"
-import { AuthContextProvider } from './AuthContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import axios from "axios";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+import Sidebar from "./components/SideBar.jsx";
 
-axios.defaults.baseURL="http://localhost:5000/api/"
+axios.defaults.baseURL = "http://localhost:5000/api/";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
+    <AuthContextProvider>
+      <Sidebar />
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
-)
+);
