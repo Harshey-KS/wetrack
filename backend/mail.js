b8f2b8c6ffc725100c5b188e369516f1e26f32f7
Hardik Garg <hardikg2907@gmail.com>
2023-11-06 09:08:10 +0530

    outh and portion done

--- backend/mail.js
+++ backend/mail.js
@@ -1,14 +1,41 @@
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 
-let mailTransporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "login",
-    user: process.env.email,
-    pass: process.env.password,
-  },
-});
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject();
+      }
+      resolve(token);
+    });
+  });
+  let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      accessToken,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+    },
+  });
+
+  return mailTransporter;
+};
 
 const sendMail = async (email, teacher, type) => {
   let details = {
@@ -20,6 +47,7 @@ const sendMail = async (email, teacher, type) => {
         Site Link: https://we-track.spit.ac.in`,
   };
   //   console.log("email= ", email);
+  let mailTransporter = await createTransporter();
   mailTransporter.sendMail(details, (err) => {
     if (err) console.log(err);
     else {
